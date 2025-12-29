a6d77827002b629f896d9f5cfc73170ca0a61b50
Naresh Kumar Rajagiri <nareshkumarrajagiri@gmail.com>
2025-12-29 22:25:35 +0530

    fix: correct background execution by stripping '&' from argv

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include<signal.h>
 
 #define MAX_LINE 1024
 #define MAX_ARGS 64
@@ -21,6 +22,8 @@ void parse_input(char *input, char **args) {
 int main() {
     char input[MAX_LINE];
     char *args[MAX_ARGS];
+    signal(SIGINT, SIG_IGN);
+
 
     while (1) {
         printf("mini-shell> ");
@@ -38,6 +41,17 @@ int main() {
 
         parse_input(input, args);
 
+        int background = 0;
+
+        for (int i = 0; args[i] != NULL; i++) {
+        if (strcmp(args[i], "&") == 0) {
+        background = 1;
+        args[i] = NULL;   // 🚨 remove '&' from argv
+        break;
+    }
+}
+
+
         /* ---- BUILT-IN COMMANDS (MUST BE BEFORE fork) ---- */
         if (strcmp(args[0], "exit") == 0) {
             break;
@@ -49,19 +63,27 @@ int main() {
             } else if (chdir(args[1]) != 0) {
                 perror("cd failed");
             }
-            continue;   // 🚨 VERY IMPORTANT
+            continue;   //  VERY IMPORTANT
         }
+
+
         /* -------------------------------------------------- */
 
         pid_t pid = fork();
+       if (pid == 0) {
+	 signal(SIGINT, SIG_DFL);   // child should die on Ctrl+C
+ 	 execvp(args[0], args);
+    	 perror("exec failed");
+   	 exit(EXIT_FAILURE);
+       }
 
-        if (pid == 0) {
-            execvp(args[0], args);
-            perror("exec failed");
-            exit(EXIT_FAILURE);
-        } 
         else if (pid > 0) {
-            waitpid(pid, NULL, 0);
+            if (!background) {
+    waitpid(pid, NULL, 0);
+} else {
+    printf("[background pid %d]\n", pid);
+}
+
         } 
         else {
             perror("fork failed");
